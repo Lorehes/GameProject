a7ca16a64b68d651dc34c80b3d62b46644df78de
Jin <lorehes4892@gmail.com>
2026-03-26 18:51:05 +0900

    refactor: restructure Hangman game logic into a class and implement word retrieval from file

--- Hangman/Hangman_logic.py
+++ Hangman/Hangman_logic.py
@@ -1,35 +1,68 @@
-from read_txt import word
 
-# A가 영어 단어를 1개 생각한다.
+import random
+import os
 
-word = word.upper()
+class HangmanGame:
+    def __init__(self, word=None):
+        if word is None:
+            word = get_random_word()
+        self.word = word.upper()
+        self.word_show = "_" * len(self.word)
+        self.try_num = 0
+        self.ok_list = []
+        self.no_list = []
+        self.max_try = 7
+        self.finished = False
+        self.won = False
 
-# 단어의 글자 수 만큼 밑줄을 긋는다.
-word_show = "_"*len(word)
+    def guess(self, ans):
+        ans = ans.upper()
+        if ans in self.ok_list or ans in self.no_list or len(ans) != 1 or not ans.isalpha():
+            return  # 이미 시도했거나 잘못된 입력
+        result = self.word.find(ans)
+        if result == -1:
+            self.try_num += 1
+            self.no_list.append(ans)
+        else:
+            self.ok_list.append(ans)
+            for i in range(len(self.word)):
+                if self.word[i] == ans:
+                    self.word_show = self.word_show[:i] + ans + self.word_show[i+1:]
+        if self.try_num >= self.max_try:
+            self.finished = True
+            self.won = False
+        elif "_" not in self.word_show:
+            self.finished = True
+            self.won = True
 
-try_num = 0
-ok_list = []
-no_list = []
+    def get_display(self):
+        return self.word_show
 
-while True:
-    # B가 단어에 포함될 것 같은 알파벳을 1개씩 제시한다.
-    ans = input().upper()
+    def get_wrong(self):
+        return self.no_list
 
-    # 알파벳이 단에에 포함된다면 밑줄에 알파벳을 채워 놓고
-    # 포함되지 않는다면 사람을 1획 씩 그린다.
-    result = word.find(ans)
-    if result == -1: # 없음
-        try_num += 1
-        no_list.append(ans)
-    else: # 있음
-        ok_list.append(ans)
-        for i in range(len(word)):
-            if word[i] == ans:
-                word_show = word_show[:i] + ans + word_show[i+1:]
-                
+    def get_right(self):
+        return self.ok_list
 
-    # 사람이 먼저 완성됨녀 출제자 A가 승리하고
-    if try_num == 7 : break
-    
-    # 단어가 먼저 완성되면 단어를 맞힌 B가 승리한다.
-    if word_show.find("_") == -1 : break
+    def is_finished(self):
+        return self.finished
+
+    def is_won(self):
+        return self.won
+
+    def get_word(self):
+        return self.word
+
+# read_txt.py에서 단어를 가져오는 함수 제공
+def get_random_word():
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "hangman_words.txt")
+    f = open(file_path, "r", encoding="utf-8")
+    raw_data = f.read()
+    f.close()
+    data_list = raw_data.split("\n")
+    data_list = [w for w in data_list if w.strip()]
+    while True:
+        r_index = random.randrange(0, len(data_list))
+        word = data_list[r_index].replace(u"\xa0", u" ").split(" ")[-1]
+        if len(word) <= 6:
+            return word
